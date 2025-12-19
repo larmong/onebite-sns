baf6c77eafe68e0b051ca2df7ad35b50924aeb04
larmong <larmong@naver.com>
2025-12-19 16:15:54 +0900

    zustand 미들웨어 => devtools > persist > subscribeWithSelector > immer > combine

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,15 +1,70 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-// combine 사용하는 이유
-// type을 자동 추론해줌
+// combine => type을 자동 추론 해줌
+// immer => update를 편리하게 해줌
+// subscribeWithSelector => select 함수를 통해서 store의 특정 값을 구독함으로써, 해당 값이 변경 될 때 마다 어떠한 기능을 수행하게 해줌
+// persist => localStorage, sessionStorage에 저장하게 해줌
+//
+// ======> devtools > persist > subscribeWithSelector > immer > combine 순서를 잘 지켜서 사용해야 함!!!
+
 export const useCountStore = create(
-  combine({ count: 0 }, (set) => ({
-    actions: {
-      increase: () => set((state) => ({ count: state.count + 1 })),
-      decrease: () => set((state) => ({ count: state.count - 1 })),
-    },
-  }))
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, (set) => ({
+            actions: {
+              increase: () => {
+                set((state) => {
+                  state.count += 1;
+                });
+
+                // set((state) => ({
+                //   count: state.count + 1
+                // }))
+              },
+              decrease: () => {
+                set((state) => {
+                  if (state.count === 0) return;
+                  state.count -= 1;
+                });
+
+                // set((state) => ({
+                //   count: state.count - 1
+                // }))
+              },
+            },
+          }))
+        )
+      ),
+      {
+        name: "countStore",
+        partialize: (store) => ({
+          count: store.count, // persist로 저장 할 값을 지정해 줌
+        }),
+        storage: createJSONStorage(() => sessionStorage), // 기본은 localStorage에 저장 됨
+      }
+    ),
+    {
+      name: "countStore",
+    }
+  )
+);
+
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    // Listner
+    console.log(`count changed! count: ${count}, prevCount: ${prevCount}`);
+  }
 );
 
 // type Store = {
