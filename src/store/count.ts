a3b4026f4aafe447baf5d9316c4348e1e3de4577
larmong <larmong@naver.com>
2025-12-19 14:39:09 +0900

    zustand 미들웨어 - combine

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,17 +1,29 @@
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
 
-type Store = {
-  count: number;
-  actions: { increase: () => void; decrease: () => void };
-};
+// combine 사용하는 이유
+// type을 자동 추론해줌
+export const useCountStore = create(
+  combine({ count: 0 }, (set) => ({
+    actions: {
+      increase: () => set((state) => ({ count: state.count + 1 })),
+      decrease: () => set((state) => ({ count: state.count - 1 })),
+    },
+  }))
+);
 
-export const useCountStore = create<Store>((set) => ({
-  count: 0,
-  actions: {
-    increase: () => set((state) => ({ count: state.count + 1 })),
-    decrease: () => set((state) => ({ count: state.count - 1 })),
-  },
-}));
+// type Store = {
+//   count: number;
+//   actions: { increase: () => void; decrease: () => void };
+// };
+
+// export const useCountStore = create<Store>((set) => ({
+//   count: 0,
+//   actions: {
+//     increase: () => set((store) => ({ count: store.count + 1 })),
+//     decrease: () => set((store) => ({ count: store.count - 1 })),
+//   },
+// }));
 
 // 이름 바뀌는것 대비해서 커스텀 훅 만들기
 // 커스텀 훅을 사용하면 더 안정적인 코드 작성 가능
